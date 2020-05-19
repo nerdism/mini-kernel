54890c045aaef885fd92d9a454e97ea81701c122
MrSmiler <00riddler00@gmail.com>
2020-05-20 01:38:50 +0430

    interrupt initialized

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,15 @@
 #include "screen.h"
 #include "printf.h"
 #include "memory.h"
+#include "interrupts.h"
 
 
 void main() {
     init_screen();    
     printf("[+] screen init\n");
     memory_init();
+
+    intr_init();
     
     // printf("free memory begins from 0x%h\n", (uint32_t)m);
     // printf("free memory begins from 0x%h\n", (uint32_t)m2);
