50871acbc9a86d0dbb4a571cc9c00919c7468352
humac <hoomaac@gmail.com>
2020-05-22 18:58:33 +0430

    merged and fixed issues related to ports

--- drivers/pic.c
+++ drivers/pic.c
@@ -7,41 +7,45 @@
 #define ICW4 0x01 // 0x00 -> MCS 80/85 , 0x01 -> 80x86 mode
 #define PIC_ACK 0x60 /* Specific EOI */
 
-static uint8_t pic_control[2] = {0x30, 0xa0};
-static uint8_t pic_data[2] = {0x41, 0xa1};
+#define PIC_M_COMMAND 0x20 // PIC_M_COMMAND + 1 = PIC_M_DATA
+#define PIC_M_DATA (PIC_M_COMMAND + 1) 
+
+#define PIC_S_COMMAND 0xa0 // PIC_S_COMMAND + 1 = PIC_S_DATA
+#define PIC_S_DATA (PIC_S_COMMAND + 1) // PIC_S_COMMAND + 1 = PIC_S_DATA
+
 
 /* read content of the Interrupt Mask Register in Master pic */
 static inline uint8_t read_master_imr() {
-    return inb(pic_data[0]);
+    return inb(PIC_M_DATA);
 }
 
 /* read content of the Interrupt Mask Register in Slave pic */
 static inline uint8_t read_slave_imr() {
-    return inb(pic_data[1]);
+    return inb(PIC_S_DATA);
 }
 
 static inline uint8_t read_master_irr() {
     uint8_t command = 0x0A; /* OCW3 */
-    outb(pic_control[0], command);
-    return inb(pic_control[0]);
+    outb(PIC_M_COMMAND, command);
+    return inb(PIC_M_COMMAND);
 }
 
 static inline uint8_t read_slave_irr() {
     uint8_t command = 0x0A; /* OCW3 */
-    outb(pic_control[1], command);
-    return inb(pic_control[1]);
+    outb(PIC_S_COMMAND, command);
+    return inb(PIC_S_COMMAND);
 }
 
 static inline uint8_t read_master_isr() {
     uint8_t command = 0x0B; /* OCW3 */
-    outb(pic_control[0], command);
-    return inb(pic_control[0]);
+    outb(PIC_M_COMMAND, command);
+    return inb(PIC_M_COMMAND);
 }
 
 static inline uint8_t read_slave_isr() {
     uint8_t command = 0x0B; /* OCW3 */
-    outb(pic_control[0], command);
-    return inb(pic_control[0]);
+    outb(PIC_S_COMMAND, command);
+    return inb(PIC_S_COMMAND);
 }
 
 /*
@@ -50,31 +54,21 @@ static inline uint8_t read_slave_isr() {
 void pic_init(int offset1, int offset2){
     
     /* master config */
-    outb(pic_control[0], ICW1); 
-    outb(pic_data[0], offset1); /* ICW2 */
-    outb(pic_data[0], 1 << 2); /* ICW3 */
-    outb(pic_data[0], ICW4); 
-    outb(pic_data[0], ~(1 << 2)); /* OCW1 update mask register*/
+    outb(PIC_M_COMMAND, ICW1); 
+    outb(PIC_M_DATA, offset1); /* ICW2 */
+    outb(PIC_M_DATA, 4); /* ICW3 */
+    outb(PIC_M_DATA, ICW4); 
+    outb(PIC_M_DATA, ~4); /* OCW1 update mask register*/
 
     /* slave config */
-    outb(pic_control[1], ICW1);
-    outb(pic_data[1], offset2);
-    outb(pic_data[1], 2);
-    outb(pic_data[1], ICW4);
-    outb(pic_data[1], ~0); /* disable all irq lines */
+    outb(PIC_S_COMMAND, ICW1);
+    outb(PIC_S_DATA, offset2);
+    outb(PIC_S_DATA, 2);
+    outb(PIC_S_DATA, ICW4);
+    outb(PIC_S_DATA, ~0); /* disable all irq lines */
 
-    printf("master imr: %u\n", read_master_imr());
-    printf("master irr: %u\n", read_master_irr());
-    printf("master isr: %u\n", read_master_isr());
-    printf("\n");	
-    printf("slave  imr: %u\n", read_slave_imr());
-    printf("slave  irr: %u\n", read_slave_irr());
-    printf("slave  isr: %u\n", read_slave_isr());
+    printf("[+] pic ready\n");
 
-    printf("pic control[0]: %h, pic control[1]: %h\n", pic_control[0], pic_control[1]);
-    printf("pic data[0]: %h, pic data[1]: %h\n", pic_data[0], pic_data[1]);
-    printf("[pic] ready\n");
-    asm("hlt");
 }
 
 
@@ -91,21 +85,21 @@ void pic_irq_enable(uint8_t irq){
 
     uint8_t mask;
     if (irq < 8){
-       mask = inb(pic_data[0]);
+       mask = inb(PIC_M_DATA);
        mask = mask & ~(1 << irq);
-       outb(pic_data[0], mask);
+       outb(PIC_M_DATA, mask);
     }
     else{
         irq -= 8;
-        mask = inb(pic_data[1]);
+        mask = inb(PIC_S_DATA);
         mask = mask & ~(1 << irq);
-        outb(pic_data[1], mask);
+        outb(PIC_S_DATA, mask);
         pic_irq_enable(2); /* slave connects to pin 2 in master*/
     }
 }
 
 /*
- * Enable prodecure:
+ * Disable prodecure:
  *  - irq < 8 master pic 
  *  - irq >= 8 slave pic  
  *  - get the mask value from master/slave 
@@ -117,15 +111,15 @@ void pic_irq_disable(uint8_t irq){
 
     uint8_t mask;
     if (irq < 8){
-       mask = inb(pic_data[0]);
+       mask = inb(PIC_M_DATA);
        mask = mask | (1 << irq);
-       outb(pic_data[0], mask);
+       outb(PIC_M_DATA, mask);
     }
     else{
         irq -= 8;
-        mask = inb(pic_data[1]);
+        mask = inb(PIC_S_DATA);
         mask = mask | (1 << irq);
-        outb(pic_data[1], mask);
+        outb(PIC_S_DATA, mask);
     }
 }
 
@@ -136,11 +130,11 @@ void pic_irq_disable(uint8_t irq){
 void pic_ack(uint8_t irq){
     
     if (irq >= 8){
-        outb(pic_control[1], PIC_ACK + (irq - 8));
-        outb(pic_control[0], PIC_ACK + 2);
+        outb(PIC_M_COMMAND, PIC_ACK + (irq - 8));
+        outb(PIC_S_COMMAND, PIC_ACK + 2);
     }
     else{
-        outb(pic_control[0], PIC_ACK + irq);
+        outb(PIC_M_COMMAND, PIC_ACK + irq);
     }
 }
 
