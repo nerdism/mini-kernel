ab60947b92012e2f612449a6639adab79a3d1cb4
humac <hoomaac@gmail.com>
2020-08-01 17:54:17 +0430

    edit scan code

--- include/drivers/keyboard.h
+++ include/drivers/keyboard.h
@@ -1,9 +1,11 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include "types.h"
 
-void init();
+uint8_t to_keycode(uint8_t scan_code);
+char keyboard_read();
 void keyboard_interrupt_handler();
-
+void keyboard_init();
 
 #endif
